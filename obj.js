91322923f961c228068242799ce870bbfedc240a
shivu150199 <shivamsinghgautam397@gmail.com>
2024-01-13 11:24:18 +0530

    obj.js

---
+++ obj.js
@@ -0,0 +1,168 @@
+// let obj={}
+
+// obj.name='sachin'
+// obj.age=20
+// obj.lastName='dwiwedi'
+// console.log(obj)
+
+// delete obj.age
+// console.log(obj)
+// delete obj.name
+// console.log(obj)
+
+// let books={
+//     name:'Ramcharit Manas',
+//     author:"Tulsidas"
+// }
+// console.log(books)
+// books.author='valmiki'
+// console.log(books)
+
+// let books={
+//     name:'Ramcharit Manas',
+//     author:"Tulsidas"
+// }
+// console.log(books)
+// //bracket notation
+// books["name"]='Ramayan'
+// books['author']='Valmiki'
+
+// console.log(books)
+
+//muti word property access
+
+// let student={
+//     "first name":'sachin'
+    
+// }
+// console.log(student["first name"])
+
+
+
+// property value short hand
+
+// function student(name,age,village){
+    
+//     let obj={
+//         name:age,
+//         age:name
+//     }
+//     return obj
+// }
+// let a=student('sachin',20)
+// console.log(a)
+// function students(age,name){
+    
+//     let obj={
+//         name,
+//         age
+//     }
+//     return obj
+// }
+// let b=students('vishal',21)
+// console.log(b)
+
+// const for=24
+// console.log(for)
+
+//   let obj={
+//         for:'shivam',
+//       let:'24'
+//     }
+//     console.log(obj["for"])
+
+
+// key in Object
+
+// let menu={
+//     veg:'potato',
+//     breakfast:'fried rice',
+//     lunch:"potato",
+//     dinner:'rajma'
+//     }
+
+// console.log("dinn" in menu)
+
+
+// Write the code, one line for each action:
+
+// Create an empty object user.
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+
+
+
+// let user={};
+// user.name='john'
+// user.surname='Smith'
+// console.log(user)
+// user.name='pete'
+// console.log(user)
+// delete user.name
+// delete user.surname
+// console.log(user)
+
+
+//for in loop
+
+
+// let menu={
+//     veg:'potato',
+//     breakfast:'fried rice',
+//     lunch:"potato",
+//     dinner:'rajma'
+//     }
+//     // menu['veg']
+//     for(let i in menu ){
+//         // console.log(i)
+//         console.log(menu[i])
+        
+        
+//     }
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
