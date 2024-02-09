968a61c6b8b0315ecaf6732696a9a75f200ca18e
shivu150199 <shivamsinghgautam397@gmail.com>
2024-02-09 17:50:56 +0530

    update

--- toast/index.js
+++ toast/index.js
@@ -14,10 +14,12 @@
 
 // login.addEventListener('click',showNotification)
 
-function showNotification(message,color){
+function showNotification(message,color,left){
+
   let div=document.createElement('div')//<div></div>
   div.className='card'
   div.style.borderBottom=`5px solid ${color}`
+  div.style.left=`${left}%`
   let p=document.createElement('p')
   p.innerText=message
   let btn=document.createElement('button')
@@ -25,6 +27,11 @@ function showNotification(message,color){
   btn.innerText='X'
   div.append(p,btn)
   devi.append(div)
+
+
+  setTimeout(()=>{
+    div.remove()
+  },2000)
 }
 
 
@@ -32,9 +39,9 @@ function showNotification(message,color){
 
 login.addEventListener('click',()=>{
     if(email.value==''||password.value==''){
-showNotification('provide value', 'red')
+showNotification('provide value', 'red',10)
     }else{
-        showNotification('login successful', 'green')
+        showNotification('login successful', 'green',20)
     }
 })
 
