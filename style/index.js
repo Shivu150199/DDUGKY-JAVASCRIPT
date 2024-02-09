1aef56d590888b878a888686574d1c13b0e723e8
shivu150199 <shivamsinghgautam397@gmail.com>
2024-02-09 17:23:42 +0530

    notification

---
+++ style/index.js
@@ -0,0 +1,34 @@
+let title = document.querySelector('h1')
+// input.addEventListener('keyup', () => {
+//   let value = input.value
+//   title.innerText = value
+// })
+
+// console.log(title.style.color)
+// title.style.color = 'red'
+// title.style.fontSize = '2rem'
+// console.log(title.style.fontSize)
+
+// let div = document.querySelector('div')
+// div.style.backgroundColor = 'aqua'
+
+// btn.addEventListener('click',()=>{
+//     let value=input.value
+//     title.style.fontSize=`${value}px`
+//     title.style.color='red'
+//     console.log(title.style)
+// })
+
+let div=document.getElementsByTagName('div')
+// console.log(div[0].className)
+// div[0].classList.add('card')
+// title.classList.remove('title')
+
+// console.log(div[0].style)
+// console.log(div[0])
+// div[0].style.cssText="border: 1px solid black;padding: 2rem;border-radius: 1rem;background-color: aliceblue;box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);"
+
+
+
+
+
---
+++ toast/index.js
@@ -0,0 +1,41 @@
+// let ksfdjdsgkl=document.querySelector('#devi')
+
+
+// let div=document.createElement('div')//<div></div>
+// div.className='card'
+// let p=document.createElement('p')
+// p.innerText='Login Success full'
+// let btn=document.createElement('button')
+// btn.id='btn'
+// btn.innerText='X'
+// div.append(p,btn)
+// devi.append(div)
+
+
+// login.addEventListener('click',showNotification)
+
+function showNotification(message,color){
+  let div=document.createElement('div')//<div></div>
+  div.className='card'
+  div.style.borderBottom=`5px solid ${color}`
+  let p=document.createElement('p')
+  p.innerText=message
+  let btn=document.createElement('button')
+  btn.id='btn'
+  btn.innerText='X'
+  div.append(p,btn)
+  devi.append(div)
+}
+
+
+
+
+login.addEventListener('click',()=>{
+    if(email.value==''||password.value==''){
+showNotification('provide value', 'red')
+    }else{
+        showNotification('login successful', 'green')
+    }
+})
+
+
