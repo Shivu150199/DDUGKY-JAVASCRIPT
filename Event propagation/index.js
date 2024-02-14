9e265a13b127e35320b26b542991130d962b3460
shivu150199 <shivamsinghgautam397@gmail.com>
2024-02-14 22:33:46 +0530

    hello

---
+++ Event propagation/index.js
@@ -0,0 +1,22 @@
+// function sayHi() {
+//   console.log('hi every one')
+// }
+// function sayNamaste() {
+//   console.log('namaste sabko')
+// }
+// function sayBonjour() {
+//   console.log('bonjour tout le monde')
+// }
+// console.log(grandParent)
+// console.log(parents)
+// // console.log(child)
+
+// grandParent.addEventListener('click', sayNamaste,true)
+// parents.addEventListener('click', sayHi,true)
+// child.addEventListener('click', sayBonjour,true)
+
+// what is event life cycle//
+
+// capturing
+// Target hit
+// bubbling
