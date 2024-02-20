1f3957adca024bfe04e490d139c7c616248b4d93
shivu150199 <shivamsinghgautam397@gmail.com>
2024-02-20 13:07:47 +0530

    complete drag function

--- jira/index.js
+++ jira/index.js
@@ -3,8 +3,13 @@ let input = document.querySelector('.issue')
 btn.addEventListener('click', () => {
   input.classList.remove('hide')
   btn.classList.add('hide')
-    input.focus()
+  input.focus()
 })
+
+let draggingInfo = {
+  draggingElement: null,
+  draggingId: null,
+}
 
 input.addEventListener('keyup', (e) => {
   let todoContainer = document.querySelector('#to-do')
@@ -18,10 +23,40 @@ input.addEventListener('keyup', (e) => {
     card.style.padding = '10px'
     card.style.borderRadius = '10px'
     card.style.marginTop = '5px'
+    card.setAttribute('data-container', 'card')
     card.style.background = 'skyblue'
+    card.draggable = 'true'
     todoContainer.append(card)
+
+    card.addEventListener('dragstart', (e) => {
+      console.log('hello')
+      draggingInfo.draggingElement = e.target
+      draggingInfo.draggingId = e.target.getAttribute('data-container')
+      console.log(draggingInfo.draggingElement, draggingInfo.draggingId)
+    })
+
     input.classList.add('hide')
     btn.classList.remove('hide')
-    input.value=''
+    input.value = ''
   }
+})
+
+let allContainer = document.querySelectorAll('.container')
+console.log(allContainer)
+
+allContainer.forEach((item) => {
+  item.addEventListener('dragover', (event) => {
+    event.preventDefault()
+  })
+
+item.addEventListener('drop',(e)=>{
+  console.log('dropping card')
+  let card=draggingInfo.draggingElement
+  card.id=draggingInfo.draggingId
+let box=e.currentTarget
+console.log(box)
+box.appendChild(card)
+
+})
+
 })
